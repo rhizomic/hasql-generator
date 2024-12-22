3d45aac656c40f7b3bd1ed0f8d26f35eba85d8af
rhizomic <>
2024-12-22 12:49:33 -0500

    Update nixpkgs, relax constraints on hasql-pool

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "hasql-generator";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
